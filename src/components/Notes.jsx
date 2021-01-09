b1ce68bb1953958a52464d8a00dc91629560dfe6
snigdhcinu <snigdhshourya11111998@gmail.com>
2021-01-10 01:38:13 +0530

    adding feature of recording date of creation, some implementation bug

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -5,6 +5,8 @@ function Note(props){
 	return <div className='note'>
 		<h1>{props.title}</h1>
 		<p>{props.content}</p>
+
+		<p className='date-created'>{props.date}</p>
 	</div>
 }
 
--- src/components/Render.jsx
+++ src/components/Render.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import Note from './Notes.jsx'
 import notes from '../notes.js'
 
+
+
+
 function Render(){
 
 		return (
 		notes.map((note)=>{
-			return <Note title={note.title} content={note.content} key={note.key} />
+			let d = new Date();
+			let year = d.getFullYear();
+			let month = d.getMonth() + 1;
+			let day = d.getDate();
+			return <Note title={note.title} content={note.content} key={note.key} date={`posted on - ${day}/${month}/${year}`} />
 		})
 		);
 
